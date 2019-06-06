0d3a61b431cc143a45c25e0572be9205dd331386
kiterumer <1404646092@qq.com>
2019-06-06 11:50:17 +0800

    完成todoItem

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,23 +1,72 @@
 import * as React from 'react';
 import TodoInput from 'src/components/Todos/TodoInput'
+import TodoItem from 'src/components/Todos/TodoItem'
 import axios from 'src/config/axios'
 import './Todos.scss'
 
-class Todos extends React.Component {
-
+interface ITodosState {
+    todos:any[]
+}
+class Todos extends React.Component<any,ITodosState> {
+    constructor(props){
+        super(props)
+        this.state = {
+            todos:[]
+        }
+    }
 	addTodo = async (params:any)=>{
+        const {todos} = this.state
 		try{
 			const response = await axios.post('todos',params)
-			console.log(response.data);
+            console.log(response.data);
+            this.setState({todos:[response.data.resource,...todos]})
 		}catch (e) {
 			throw new Error(e)
 		}
-	}
+    }
+    
+    componentDidMount(){
+        this.getTodos()
+    }
 
+    getTodos = async () => {
+        try{
+            const response = await axios.get('todos')
+            console.log(response.data)
+            this.setState({todos:response.data.resources})
+        }catch(e){
+            throw new Error(e)
+        }
+    }
+
+    updateTodo = async (id:number,params:any) => {
+        const {todos} = this.state
+        try{
+            const response = await axios.put(`todos/${id}`,params)
+            const newTodos = todos.map(t => {
+                if(id === t.id){
+                    return response.data.resource
+                }else{
+                    return t
+                }
+            })
+            this.setState({todos:newTodos})
+        }catch(e){
+            throw Error(e)
+        }
+    }
 	public render() {
 		return (
 			<div className="Todos" id="Todos">
 				<TodoInput addTodo={(params)=>this.addTodo(params)}/>
+                <main>
+                    {
+                        this.state.todos.map(t => <TodoItem 
+                        key={t.id} {...t}
+                        update={this.updateTodo}                        
+                        />)
+                    }
+                </main>
 			</div>
 		);
 	}
